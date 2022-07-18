136957861b286d64cb021c24f5a4fdb864ace902
Tommaso Meledina <tommaso.meledina@it.ibm.com>
2022-07-18 02:03:28 +0200

    feat: make generated project work ootb

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -3,6 +3,16 @@ file = new File( request.getOutputDirectory(), request.getArtifactId()+"/.gitign
 def gitIgnorefile = new File( request.getOutputDirectory(), request.getArtifactId()+"/.gitignore" );
 file.renameTo(gitIgnorefile)
 
+System.out.println("Initializing git:\n"+request.getVersion())
+def initProcess = new ProcessBuilder("git","init").directory(new File(request.getArtifactId())).start()
+initProcess.waitForProcessOutput(System.out, System.err)
+def initialAddProcess = new ProcessBuilder("git","add",".").directory(new File(request.getArtifactId())).start()
+initialAddProcess.waitForProcessOutput(System.out, System.err)
+def initialCommitProcess = new ProcessBuilder("git","commit","-m","chore: initial commit").directory(new File(request.getArtifactId())).start()
+initialCommitProcess.waitForProcessOutput(System.out, System.err)
+def tagProcess = new ProcessBuilder("git","tag", request.getVersion()).directory(new File(request.getArtifactId())).start()
+tagProcess.waitForProcessOutput(System.out, System.err)
+
 def activeProfiles = request.getProjectBuildingRequest().getActiveProfileIds()
 def isWindows = false
 def isUnix = false
@@ -12,24 +22,24 @@ for (i=0;i<activeProfiles.size();i++) {
     if(activeProfiles[i].equals("unix-local-build"))
         isUnix=true
 }
-def mavenPackageProcess
+def mavenInstallProcess
 if(isWindows && isUnix) {
     System.out.println("BOTH windows and UNIX profiles? Check your config...")
     System.exit(1)
 }
 else if(isWindows) {
-    System.out.println("Executing 'mvn package -DskipTests=true' in directory "+request.getArtifactId())
-    mavenPackageProcess = new ProcessBuilder("mvn.cmd","package","-DskipTests=true","-P e2e-build unix-local-build").directory(new File(request.getArtifactId())).start()
+    System.out.println("Executing 'mvn install -DskipTests=true -P e2e-build,windows-local-build' in directory "+request.getArtifactId())
+    mavenInstallProcess = new ProcessBuilder("mvn.cmd","install","-DskipTests=true","-P e2e-build,windows-local-build").directory(new File(request.getArtifactId())).start()
 }
 else if(isUnix) {
-    System.out.println("Executing 'mvn package -DskipTests=true' in directory "+request.getArtifactId())
-    mavenPackageProcess = new ProcessBuilder("mvn","package","-DskipTests=true","-P e2e-build windows-local-build").directory(new File(request.getArtifactId())).start()
+    System.out.println("Executing 'mvn install -DskipTests=true -P e2e-build,unix-local-build' in directory "+request.getArtifactId())
+    mavenInstallProcess = new ProcessBuilder("mvn","install","-DskipTests=true","-P e2e-build,unix-local-build").directory(new File(request.getArtifactId())).start()
 }
 else {
     System.out.println("NEITHER windows NOR UNIX profiles? Check your config...")
     System.exit(1)
 }
-mavenPackageProcess.waitForProcessOutput(System.out, System.err)
+mavenInstallProcess.waitForProcessOutput(System.out, System.err)
 
 /*
 System.out.println("Executing 'mvn clean' in directory "+request.getArtifactId())
@@ -53,12 +63,10 @@ else {
 mavenCleanProcess.waitForProcessOutput(System.out, System.err)
 */
 
-System.out.println("Initializing git")
-def initProcess = new ProcessBuilder("git","init").directory(new File(request.getArtifactId())).start()
-initProcess.waitForProcessOutput(System.out, System.err)
+System.out.println("Adding to git")
 def addProcess = new ProcessBuilder("git","add",".").directory(new File(request.getArtifactId())).start()
 addProcess.waitForProcessOutput(System.out, System.err)
-def commitProcess = new ProcessBuilder("git","commit","-m","chore: initial commit").directory(new File(request.getArtifactId())).start()
+def commitProcess = new ProcessBuilder("git","commit","-m","chore: complete first-time setup").directory(new File(request.getArtifactId())).start()
 commitProcess.waitForProcessOutput(System.out, System.err)
 
 System.out.println("Done!")
